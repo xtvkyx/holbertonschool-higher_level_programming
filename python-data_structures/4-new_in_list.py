ac1667894d6646b94a9284870ec5e6065d9e679d
“xtvkyx” <ljabli@icloud.com>
2025-12-02 15:20:35 +0000

    4. Replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,8 +3,8 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
-        new_list = [ ]
-    for element in my_list:
-        new_list.append(element)
-    new_list[idx] = element
-    return my_list
+        new_list = []
+        for elem in my_list:
+            new_list.append(elem)
+        new_list[idx] = element
+        return new_list
