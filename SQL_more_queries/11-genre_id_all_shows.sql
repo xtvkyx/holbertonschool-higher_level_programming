6446415e7cfcc8082b496afee9d671f34b39bb37
Lamisfaljabli <ljabli@icloud.com>
2026-01-18 00:51:11 +0300

    task11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Lists all shows and their genre IDs (NULL if no genre)
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
